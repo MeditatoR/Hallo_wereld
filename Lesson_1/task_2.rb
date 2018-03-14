a4a57f5dc361dcf24faf9789c1af2115a1a3d81f
Mediator <you@example.com>
2018-03-14 23:51:04 +0300

    Lesson 1-исправления

--- Lesson_1/task_2.rb
+++ Lesson_1/task_2.rb
@@ -1,6 +1,6 @@
 puts "Введите основание треугольника"
-a = gets.chomp.to_i
+a = gets.chomp.to_f
 puts "Введите высоту треугольника"
-h = gets.chomp.to_i
+h = gets.chomp.to_f
 s = a * h * 1 / 2
-puts "Площадь треугольника равна #{a * h * 1 / 2.to_f}"
+puts "Площадь треугольника равна #{s}"
--- Lesson_1/task_4.rb
+++ Lesson_1/task_4.rb
@@ -1,15 +1,17 @@
 puts "Введите 3 коэффицента"
-a = gets.to_i
-b = gets.to_i
-c = gets.to_i
+a = gets.to_f
+b = gets.to_f
+c = gets.to_f
 
-d = (b ** 2) - (4 * a * c)
+d = b**2 - 4 * a * c
 
+x1 = (-b + Math.sqrt(d)) / (2 * a)
+x2 = (-b - Math.sqrt(d)) / (2 * a)
 puts "Дискриминант = #{d}"
 
 if d > 0
-	puts "x1 = #{(-b + Math.sqrt(d)) / (2 * a)}"
-	puts "x2 = #{(-b - Math.sqrt(d)) / (2 * a)}"
+	puts "x1 = #{x1}"
+	puts "x2 = #{x2}"
   elsif d == 0
 	puts "x = #{(-b / (2 * a))}"
   else
