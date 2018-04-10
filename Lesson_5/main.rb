93ad21fa232747d8f072fbd2f473f5f1cdd44230
Mediator <you@example.com>
2018-04-10 16:48:32 +0300

    Lesson-5_private_methods

--- Lesson_5/main.rb
+++ Lesson_5/main.rb
@@ -19,14 +19,42 @@ class Main
   end
 
 
-  def create_station
-    puts 'Введите название станции:'
-    name = gets.chomp
-    stations << Station.new(name)
+  def menu
+    input = 1
+    while input != 0
+      puts 'Выберете действие
+         1.- Создавать станции
+         2.- Создавать поезда
+         3.- Создавать маршруты и управлять станциями в нем (добавлять, удалять)
+         4.- Назначать маршрут поезду
+         5.- Добавлять вагоны к поезду
+         6.- Отцеплять вагоны от поезда
+         7.- Перемещать поезд по маршруту вперед и назад
+         8.- Просматривать список станций и список поездов на станции
+
+         0 - ВЫХОД'
+
+      input = gets.chomp.to_i
+
+      case input
+        when 1 then create_station
+        when 2 then create_train
+        when 3 then route_menu
+        when 4 then add_route_to_train
+        when 5 then add_wagon
+        when 6 then remove_wagon
+        when 7 then movement_menu
+        when 8 then station_info
+        when 0 then puts 'Конец'
+        else
+          puts 'Данная операция не предусмотрена'
+        end
+    end
   end
 
+private
 
-  def create_train
+def create_train
     puts 'Введите номер поезда:'
     number = gets.chomp
     puts 'Выберите тип поезда:
@@ -37,6 +65,13 @@ class Main
       when 1 then trains << PassengerTrain.new(number)
       when 2 then trains << CargoTrain.new(number)
     end
+  end
+
+
+def create_station
+    puts 'Введите название станции:'
+    name = gets.chomp
+    stations << Station.new(name)
   end
 
 
@@ -90,6 +125,12 @@ class Main
     train.detach_wagon
   end
 
+  def add_route_station
+    route = route_select
+    station = station_select
+    route.add_station(station)
+    puts " #{route.print_route}"
+  end
 
   def route_menu
     puts 'Управление маршрутами:
@@ -108,15 +149,6 @@ class Main
       else puts 'Действие невозможно'
     end
   end
-
-
-  def add_route_station
-    route = route_select
-    station = station_select
-    route.add_station(station)
-    puts " #{route.print_route}"
-  end
-
 
   def remove_route_station
     route = route_select
@@ -170,39 +202,6 @@ class Main
     train.set_route(route)
   end
 
-
-  def menu
-    input = 1
-    while input != 0
-      puts 'Выберете действие
-         1.- Создавать станции
-         2.- Создавать поезда
-         3.- Создавать маршруты и управлять станциями в нем (добавлять, удалять)
-         4.- Назначать маршрут поезду
-         5.- Добавлять вагоны к поезду
-         6.- Отцеплять вагоны от поезда
-         7.- Перемещать поезд по маршруту вперед и назад
-         8.- Просматривать список станций и список поездов на станции
-
-         0 - ВЫХОД'
-
-      input = gets.chomp.to_i
-
-      case input
-        when 1 then create_station
-        when 2 then create_train
-        when 3 then route_menu
-        when 4 then add_route_to_train
-        when 5 then add_wagon
-        when 6 then remove_wagon
-        when 7 then movement_menu
-        when 8 then station_info
-        when 0 then puts 'Конец'
-        else
-          puts 'Данная операция не предусмотрена'
-        end
-    end
-  end
 
   puts "\e[31m MAIN RB!\e[0m"
 end
